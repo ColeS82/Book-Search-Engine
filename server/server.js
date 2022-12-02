8a0e960da290e4aacbb2c82abc3ab2bcb3233153
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-12-02 16:59:08 -0700

    added schemas directory

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
+
+//const routes = require('./routes');
+
+//importing ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+//importing typeDefs
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware = require('./utils/auth')};
+
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+
+server.applyMiddleware({ app })
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
